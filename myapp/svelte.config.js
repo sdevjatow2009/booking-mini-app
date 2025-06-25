be030c201bf9affafbc3e48289b4b68e817e445d
Krram <kokokola.beach@gmail.com>
2025-06-25 13:39:28 +0200

    added github page

--- myapp/svelte.config.js
+++ myapp/svelte.config.js
@@ -3,11 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 export default {
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
+			fallback: 'index.html' 
 		})
 	}
 };
