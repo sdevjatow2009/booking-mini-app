039a608d6a79e4b6adc89249fc7a8b409ab4aec0
SD <dis86911@gmail.com>
2025-07-02 09:01:06 +0200

    .nojekyll

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["background.png","booking.ico","donut.png","favicon.png","french-fries.png","golf.png","hamburger.png","icon.ico","img.png","pizza.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {start:"_app/immutable/entry/start.DROp3PbP.js",app:"_app/immutable/entry/app.Dev02ZUR.js",imports:["_app/immutable/entry/start.DROp3PbP.js","_app/immutable/chunks/BpC9hT6p.js","_app/immutable/chunks/CMWCWS23.js","_app/immutable/chunks/CV0WbJ7o.js","_app/immutable/entry/app.Dev02ZUR.js","_app/immutable/chunks/CMWCWS23.js","_app/immutable/chunks/B447aQBC.js","_app/immutable/chunks/C86OkpKZ.js","_app/immutable/chunks/CV0WbJ7o.js","_app/immutable/chunks/C1BndpW2.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.DvXsTZBQ.js",app:"_app/immutable/entry/app.D0P-9JWK.js",imports:["_app/immutable/entry/start.DvXsTZBQ.js","_app/immutable/chunks/DMecHtFy.js","_app/immutable/chunks/CMWCWS23.js","_app/immutable/chunks/CV0WbJ7o.js","_app/immutable/entry/app.D0P-9JWK.js","_app/immutable/chunks/CMWCWS23.js","_app/immutable/chunks/B447aQBC.js","_app/immutable/chunks/C86OkpKZ.js","_app/immutable/chunks/CV0WbJ7o.js","_app/immutable/chunks/C1BndpW2.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
