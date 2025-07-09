1ac8e6f5fe8415e3654a0d0d174b51a4a1fc96cc
Krram <kokokola.beach@gmail.com>
2025-07-09 14:18:31 +0200

    ready project for DockerHub

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 
 export default {
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html' 
-		})
+		adapter: adapter()
 	}
 };
