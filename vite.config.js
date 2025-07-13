4b39b0269f89dbd2cf24bc91f0741b2c02d8bf77
SD <dis86911@gmail.com>
2025-07-13 08:28:50 +0200

    allow to preview on grok

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()]
+	plugins: [sveltekit(), devtoolsJson()],
+	preview: {
+		allowedHosts: ['7a7d3600da07.ngrok-free.app', 'localhost'],
+	},
 });
